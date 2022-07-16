bb3bae9db9fd932710e7becf0fa8ee2d6a62ccdd
Jihoon Oh <jihoon.oh@edu.sait.ca>
2022-07-16 11:11:07 -0600

    commit2

---
+++ src/components/ProjectList.js
@@ -0,0 +1,103 @@
+import Newtap from "../../hooks/NewTap";
+
+export const ProjectsList = [
+  // {
+  //   title: "Business Startup",
+  //   description: "Design & Development",
+  //   imgUrl: projImg1,
+  // },
+
+  {
+    id: 1,
+    type: "All",
+    title: "",
+    language: "JavaScript",
+
+    img: require("../../../assets/bubble_Game.png"),
+    link: (
+      <button onClick={() => Newtap("Mailto:Jihoon0324@hotmail.com")}>
+        Restaurant Code
+      </button>
+    ),
+  },
+  {
+    id: 5,
+    type: "none",
+    title: "Restaurant Menu",
+    language: "React",
+    img: require("../../../assets//tetris.png"),
+    link: (
+      <button
+        onClick={() => Newtap("https://github.com/jihoon0324/Tetris_javascrip")}
+      >
+        Restaurant Menu Code
+      </button>
+    ),
+  },
+  {
+    id: 6,
+    type: "none",
+    title: "Restaurant Menu",
+    language: "Javascript",
+    img: require("../../../assets//tetris.png"),
+    link: (
+      <button
+        onClick={() => Newtap("https://github.com/jihoon0324/Tetris_javascrip")}
+      >
+        Restaurant Menu Code
+      </button>
+    ),
+  },
+  {
+    id: 2,
+    type: "All",
+    title: "Tetris",
+    language: "Javascript",
+
+    img: require("../../../assets//tetris.png"),
+    link: (
+      <button
+        onClick={() => Newtap("https://github.com/jihoon0324/Tetris_javascrip")}
+      >
+        JavaScript Code
+      </button>
+    ),
+
+    link2: (
+      <button onClick={() => Newtap("https://github.com/jihoon0324/")}>
+        JavaScript Code
+      </button>
+    ),
+  },
+
+  {
+    id: 3,
+    type: "All",
+    title: "Blog",
+    language: "React",
+    img: require("../../../assets/blog.png"),
+    link: (
+      <button onClick={() => Newtap("https://github.com/jihoon0324/blog")}>
+        Blog code
+      </button>
+    ),
+  },
+  {
+    id: 4,
+    type: "All",
+    title: "Bubble Game",
+    language: "Java",
+    img: require("../../../assets/bubble_Game.png"),
+    link: (
+      <button
+        onClick={() =>
+          Newtap("https://github.com/jihoon0324/BubbleGame_Swing_Java")
+        }
+      >
+        Bubble Game Code
+      </button>
+    ),
+  },
+];
+
+// last id 6
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,46 +1,65 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import Newtap from "../components/hooks/NewTap";
 export const Projects = () => {
+  const ProjectsList = [
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg1,
+    // },
 
-  const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      id: 1,
+      type: "All",
+      title: "",
+      language: "JavaScript",
+
+      img: require("../assets/img/bubble_Game.png"),
+      link: (
+        <button onClick={() => Newtap("Mailto:Jihoon0324@hotmail.com")}>
+          Restaurant Code
+        </button>
+      ),
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      id: 5,
+      type: "All",
+      title: "Restaurant Menu",
+      language: "React",
+      img: require("../assets/img/tetris.png"),
+      link: (
+        <button
+          onClick={() =>
+            Newtap("https://github.com/jihoon0324/Tetris_javascrip")
+          }
+        >
+          Restaurant Menu Code
+        </button>
+      ),
     },
   ];
+
+  function filter_test(props) {
+    return ProjectsList.filter(
+      (Portfolio_List) => Portfolio_List.language === props
+    ).map((portfolio) => (
+      <ul className="list_component hover" key={portfolio.id}>
+        <li>
+          {portfolio.title} <br />
+          {portfolio.language}
+          <br />
+          <img src={portfolio.img} />
+          {portfolio.link}
+          <br />
+        </li>
+      </ul>
+    ));
+  }
 
   return (
     <section className="project" id="projects">
@@ -48,51 +67,70 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">All</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                      <Tab.Pane eventKey="first">
+                        <Col size={12} sm={6} md={4}>
+                          {ProjectsList.filter(
+                            (Portfolio_List) => Portfolio_List.type === "All"
+                          ).map((portfolio) => (
+                            <ul className="proj-imgbx" key={portfolio.id}>
+                              <li>
+                                {portfolio.title} <br />
+                                {portfolio.language}
+                                <br />
+                                <img src={portfolio.img} />
+                                {portfolio.link} &nbsp;
+                                {portfolio.link2}
+                              </li>
+                            </ul>
+                          ))}
+                        </Col>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>pppppppppppppppppppp</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
